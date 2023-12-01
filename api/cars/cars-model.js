d652665787b4f5b741fdb4cc758ea2a434793e15
Justin Byrd <justinbyrd7@gmail.com>
2023-12-01 10:36:19 -0600

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -2,6 +2,7 @@ const db = require("../../data/db-config");
 
 const getAll = () => {
   // DO YOUR MAGIC
+  return db("cars");
 };
 
 const getById = () => {
@@ -10,4 +11,10 @@ const getById = () => {
 
 const create = () => {
   // DO YOUR MAGIC
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 };
