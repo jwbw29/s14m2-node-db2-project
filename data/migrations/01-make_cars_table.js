99e845fb74fbed4f9c7a4a7c98b4ea9274cc1a87
Justin Byrd <justinbyrd7@gmail.com>
2023-12-01 09:08:51 -0600

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments();
+    tbl.string("vin", 17).unique().notNullable();
+    tbl.string("make", 128).notNullable();
+    tbl.string("model", 128).notNullable();
+    tbl.numeric("mileage").unsigned().notNullable(); //unsigned = not negative
+    tbl.string("title", 128);
+    tbl.string("transmission", 128);
+  });
 };
 
 exports.down = function (knex) {
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 100000,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "corola",
+    mileage: 111111,
+    title: "salvage",
+  },
+  {
+    vin: "1111111111111",
+    make: "Ford",
+    model: "F-150",
+    mileage: 30000,
+  },
+];
+
+// exports.seed = function (knex) {
+//   return knex("cars")
+//     .truncate()
+//     .then(() => {
+//       return knex("cars").insert(cars);
+//     });
+// };
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
